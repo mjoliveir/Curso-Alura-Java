72809e333f28fe5a420f29e14ab4b166a665cd89
mjoliveir <32xxthunderxx32@gmail.com>
2025-09-11 15:08:16 -0300

    Script de tabuada, exemplificação do for

---
+++ src/TabuadaExercicio.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TabuadaExercicio {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int number = reader.nextInt();
+        for (int i = 0 ; i <= 10 ; i++){
+            int value = i * number;
+            System.out.printf("%d X %d = %d%n", i, number, value);
+        }
+    }
+}
