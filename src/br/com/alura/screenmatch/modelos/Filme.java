3366439f683036338a9a871902fe9062ea39dadb
mjoliveir <32xxthunderxx32@gmail.com>
2025-09-12 13:50:07 -0300

    reestrutura dos arquivos principais da aplicação

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,4 +1,72 @@
 package br.com.alura.screenmatch.modelos;
 
-public class FIlme {
+import java.util.Scanner;
+
+public class Filme {
+        private String nome;
+        private int anoDeLancamento;
+        private boolean incluidoNoPlano;
+        private double avaliacao = 0;
+        private int totalDeAvaliacao = 0;
+        private double mediaAvaliacoes;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public void setTotalDeAvaliacao(int totalDeAvaliacao) {
+        this.totalDeAvaliacao = totalDeAvaliacao;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public int getTotalDeAvaliacao() {
+        return totalDeAvaliacao;
+    }
+
+
+    public void exibirFichaTecnica() {
+            System.out.println(nome);
+            System.out.println(anoDeLancamento);
+            System.out.println((incluidoNoPlano) ? "ativo no plano" : "inativo no plano");
+            System.out.println(avaliacao);
+            System.out.println(totalDeAvaliacao);
+        }
+
+        public void avaliar(){
+            Scanner reader = new Scanner(System.in);
+            this.avaliacao += reader.nextInt();
+            this.totalDeAvaliacao++;
+        }
+
+        public void getMedia(){
+            System.out.println(this.avaliacao / this.totalDeAvaliacao);
+        }
 }
--- src/br/com/alura/screenmatch/modelos/MainFIlme.java
+++ src/br/com/alura/screenmatch/modelos/MainFIlme.java
@@ -1,12 +1,22 @@
+package br.com.alura.screenmatch.modelos;
+
 public class MainFIlme {
     public static void main(String[] args) {
         Filme meufilme = new Filme();
-        meufilme.nome = "Harry Potter";
-        meufilme.anoDeLancamento= 1980;
-        meufilme.incluidoNoPlano = true;
-        meufilme.avaliacoes = 8.1;
-        meufilme.totalDeAvaliacao = 1000;
-
+//        meufilme.nome = "Harry Potter";
+//        meufilme.anoDeLancamento= 1980;
+//        meufilme.incluidoNoPlano = true;
+//        meufilme.avaliacoes = 8.1;
+//        meufilme.totalDeAvaliacao = 1000;
+        meufilme.setNome("Harry Potter");
+        meufilme.setAnoDeLancamento(1980);
+        meufilme.setIncluidoNoPlano(true);
         meufilme.exibirFichaTecnica();
+
+        while(true) {
+            meufilme.avaliar();
+            meufilme.getMedia();
+        }
     }
+
 }
