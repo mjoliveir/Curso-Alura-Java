3f23f88d28def88aaf217093ea2ccd73b04dbe8f
mjoliveir <32xxthunderxx32@gmail.com>
2025-09-15 16:38:25 -0300

    Exercicios de array list e generics

--- src/Exercicios/arraylist/Pessoa.java
+++ src/Exercicios/arraylist/Pessoa.java
@@ -1,4 +1,19 @@
 package Exercicios.arraylist;
 
 public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
 }
--- src/Exercicios/arraylist/main.java
+++ src/Exercicios/arraylist/main.java
@@ -1,4 +1,26 @@
 package Exercicios.arraylist;
 
+import java.util.ArrayList;
+
 public class main {
+    public static void main(String[] args) {
+        var p1 = new Pessoa("matheus", 20);
+        var p2 = new Pessoa("Arthur", 20 );
+        var p3 = new Pessoa("Giovanna", 20);
+        var p4 = new Pessoa("Danny", 20);
+        var p5 = new Pessoa("Leonardo", 20);
+        var p6 = new Pessoa("Raphael", 20);
+
+        ArrayList<Pessoa> listaDePessoas = new ArrayList<>();
+        listaDePessoas.add(p1);
+        listaDePessoas.add(p2);
+        listaDePessoas.add(p3);
+        listaDePessoas.add(p4);
+        listaDePessoas.add(p5);
+        listaDePessoas.add(p6);
+
+        System.out.println(listaDePessoas);
+
+
+    }
 }
